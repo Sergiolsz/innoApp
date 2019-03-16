058c3418ca775e2a3adc9774bd0d5f2cc6de1ed9
sergiolsz <sergiolsz82@gmail.com>
2019-03-16 12:17:08 +0100

    Create repository

---
+++ src/main/java/innocv/InnocvApplication.java
@@ -0,0 +1,13 @@
+package innocv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnocvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnocvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/innocv/InnocvApplicationTests.java
@@ -0,0 +1,16 @@
+package innocv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InnocvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
