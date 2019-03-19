0c270dc72aa0fc0a4d5341ec710a7bb010581502
sergiolsz <sergiolsz82@gmail.com>
2019-03-19 17:40:22 +0100

    Refactorized pom and DataSourceConfig

--- src/main/java/innocv/general/configuration/DataSourceConfig.java
+++ src/main/java/innocv/general/configuration/DataSourceConfig.java
@@ -4,11 +4,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Component;
 
-@Configuration
 @Component
 public class DataSourceConfig {
 
